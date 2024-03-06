060a3e775099ed3d69fd41cfe0b1d9df5173d000
picked2407 <155774081+picked2407@users.noreply.github.com>
2024-03-07 01:17:36 +0530

    Update Benefits.jsx

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -33,9 +33,6 @@ const Benefits = () => {
                     height={48}
                     alt={item.title}
                   />
-                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
-                    Explore more
-                  </p>
                   <Arrow />
                 </div>
               </div>
