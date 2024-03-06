93e6e75c25949ee187a21a948fdbd446b428934e
picked2407 <155774081+picked2407@users.noreply.github.com>
2024-03-07 01:12:29 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-8"> {/* Added mb-8 for margin-bottom */}
-            Welcome&nbsp;&nbsp;to Capital Connections {` `}
+            It is not what you know&nbsp;&nbsp;it is who you know {` `}
             <span className="inline-block relative">
               <img
                 src={curve}
@@ -29,7 +29,7 @@ const Hero = () => {
             </span>
           </h1>
           <p className="body-1 max-w-3xl mx-auto mb-15 text-n-2 lg:mb-8">
-            Elevate your professional journey by joining our exclusive community. Forge meaningful connections with successful individuals, opening doors to potential clients, partners, and investors. Experience elite networking that goes beyond traditional boundaries. 
+Elevate your professional journey by joining our exclusive community. Forge meaningful connections with successful individuals, opening doors to potential clients, partners, and investors. Experience elite networking that goes beyond traditional boundaries. - > Elevate your success journey by joining our exclusive community. Get in touch with meaningful connections, including successful traders of all kind, business/agency owners, biohackers. Open doors to potential clients, partners, and investors. Experience elite networking that goes beyond traditional boundaries.
           </p>
           <Button href="https://discord.gg/kZaWfZHRY3" white>
             Join Now
