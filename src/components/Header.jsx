6899015e4dba28a92a01bb7d5a6144778128106b
picked2407 <155774081+picked2407@users.noreply.github.com>
2024-03-07 22:13:57 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,9 +36,10 @@ const Header = () => {
       }`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        <a className="block w-[12rem] xl:mr-8" href="#hero">
-          <img src="https://media.discordapp.net/attachments/1193183717548638301/1214844370919096330/image-removebg-preview_7.png?ex=65fa96fc&is=65e821fc&hm=fd02a42ad3f85d1949f41e97833dfb7486f0e4d8e8be61eebc272982ef855f35&=&format=webp&quality=lossless&width=446&height=452" width={90} height={40} alt="Brainwave" />
-        </a>
+       <a className="block w-[12rem] xl:mr-8 flex items-center" href="#hero">
+  <img src="https://media.discordapp.net/attachments/1193183717548638301/1214844370919096330/image-removebg-preview_7.png?ex=65fa96fc&is=65e821fc&hm=fd02a42ad3f85d1949f41e97833dfb7486f0e4d8e8be61eebc272982ef855f35&=&format=webp&quality=lossless&width=446&height=452" width={40} height={40} alt="Capital Connections" style={{ marginRight: '0.5rem' }} />
+  <span>Capital Connections</span>
+</a>
 
         <nav
           className={`${
