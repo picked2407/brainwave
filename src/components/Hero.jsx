13a4ab981141d94d1c3a28a8cf0204bd536a952b
picked2407 <155774081+picked2407@users.noreply.github.com>
2024-03-08 00:09:17 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,18 +16,19 @@ const Hero = () => {
     >
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
-          <h1 className="h1 mb-8"> {/* Added mb-8 for margin-bottom */}
-            It’s not what you know,&nbsp;&nbsp;It's who you know {` `}
-            <span className="inline-block relative">
-              <img
-                src={curve}
-                className="absolute top-full left-0 w-full xl:-mt-2"
-                width={624}
-                height={28}
-                alt="Curve"
-              />
-            </span>
-          </h1>
+            <h1 className="h1 mb-8" style={{ fontFamily: 'Merriweather' }}>
+      {/* Added mb-8 for margin-bottom */}
+      It’s not what you know,&nbsp;&nbsp;It's who you know {` `}
+      <span className="inline-block relative">
+        <img
+          src={curve}
+          className="absolute top-full left-0 w-full xl:-mt-2"
+          width={624}
+          height={28}
+          alt="Curve"
+        />
+      </span>
+    </h1>
           <p className="body-1 max-w-3xl mx-auto mb-15 text-n-2 lg:mb-8">
           Welcome to Capital Connections. We believe networking is the most powerful tool because it unlocks access to opportunities, exchange of knowledge, enhances visibility, recognition, and provides guidance. By connecting with others, you can have access to a broader range of resources; accelerate personal and professional growth and strengthen relationships. All of these components increases the chances of success by lining the right people in opportunities, making network a valuable asset.</p>
           <Button href="https://discord.gg/kZaWfZHRY3" white>
