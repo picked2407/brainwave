f7e4562a1379e77372042134f12ae3a7f1a80e5a
picked2407 <155774081+picked2407@users.noreply.github.com>
2024-03-07 23:36:59 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -110,6 +110,9 @@ export const roadmap = [
   },
 ];
 
+export const collabText =
+  "With smart automation and top-notch security, it's the perfect solution for teams looking to work smarter.";
+
 export const collabContent = [
   {
     id: "0",
