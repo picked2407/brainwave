5cdc964ff6374ce175fc5e229cd5b450414f3a92
picked2407 <155774081+picked2407@users.noreply.github.com>
2024-03-07 15:50:27 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,13 @@ const Footer = () => {
   return (
     <Section crosses className="!px-0 !py-10">
       <div className="container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col">
-        <p className="caption text-n-4 lg:block">
-          © {new Date().getFullYear()}.<a href="https://sitecraftdynamics.com">Powered By SiteCraftDynamics</a>
-        </p>
+      <p className="caption text-n-4 lg:block">
+  © {new Date().getFullYear()}.
+  <a href="https://sitecraftdynamics.com">Powered By SiteCraftDynamics</a>
+  <br>
+  For inquiries, contact <a href="mailto:Capital.connectionssllc@gmail.com">Capital.connectionssllc@gmail.com</a>.
+</p>
+
 
         <ul className="flex gap-5 flex-wrap">
           {socials.map((item) => (
