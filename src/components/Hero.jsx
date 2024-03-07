226d0221d465b4aef1202681dfc110c94547b06b
picked2407 <155774081+picked2407@users.noreply.github.com>
2024-03-07 14:41:03 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
             </span>
           </h1>
           <p className="body-1 max-w-3xl mx-auto mb-15 text-n-2 lg:mb-8">
-Elevate your success journey by joining our exclusive community. Get in touch with meaningful connections, including successful traders of all kind, business/agency owners, biohackers. Open doors to potential clients, partners, and investors. Experience elite networking that goes beyond traditional boundaries.          </p>
+Welcome to capital connections. We believe networking is the most powerful tool, because it unlocks access to opportunities, exchange of knowledge, enhances, visibility and recognition, and provides guidance. By connecting with others, you can have access to a broader range of resources, accelerate, personal and professional growth, and strengthen relationships. All of these components increases the chances of success by lining the right people in opportunities, making network a valuable asset. “#
           <Button href="https://discord.gg/kZaWfZHRY3" white>
             Join Now
           </Button>
