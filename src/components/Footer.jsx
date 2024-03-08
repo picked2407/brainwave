a7daf131fdca86cbf0cc8e480f7f00a277cc2692
picked2407 <155774081+picked2407@users.noreply.github.com>
2024-03-08 19:36:49 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { socials } from "../constants";
 
 const Footer = () => {
   return (
-    <Section crosses className="!px-0 !py-10">
+    <Section crosses className="!px-0 !py-10" id="footer">
       <div className="container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col">
         <p className="caption text-n-4 lg:block">
           For inquiries, contact <a href="mailto:Capital.connectionssllc@gmail.com" className="text-white">Capital.connectionssllc@gmail.com</a>.
