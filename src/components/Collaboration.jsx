d86821ab1b2a4d81292002cf7b4461f4f58d1f3f
picked2407 <155774081+picked2407@users.noreply.github.com>
2024-03-26 13:45:40 +0530

    Update Collaboration.jsx

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -6,7 +6,7 @@ import { LeftCurve, RightCurve } from "./design/Collaboration";
 
 const Collaboration = () => {
   return (
-    <Section crosses id="collaboration">
+    <Section crosses>
       <div className="container lg:flex">
         <div className="max-w-[25rem]">
           <h2 className="h2 mb-4 md:mb-8">
@@ -27,7 +27,7 @@ const Collaboration = () => {
             ))}
           </ul>
 
-          <Button href="https://whop.com/capital-connections/">Purchase Premium Club</Button>
+          <Button href='https://whop.com/capital-connections'>Purchase Premium Club</Button>
         </div>
 
         <div className="lg:ml-auto xl:w-[38rem] mt-4">
@@ -35,7 +35,7 @@ const Collaboration = () => {
             {collabText}
           </p>
 
-          <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale-75 md:scale-100">
+          <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
             <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
               <div className="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient rounded-full">
                 <div className="flex items-center justify-center w-full h-full bg-n-8 rounded-full">
@@ -49,20 +49,27 @@ const Collaboration = () => {
               </div>
             </div>
 
-            <ul className="absolute top-0 left-1/2 w-full h-full flex items-center justify-center pointer-events-none">
+            <ul>
               {collabApps.map((app, index) => (
                 <li
                   key={app.id}
-                  className={`absolute top-1/2 left-1/2 w-[3.2rem] h-[3.2rem] bg-n-7 border border-n-1/15 rounded-xl -translate-x-1/2 -translate-y-1/2 transform rotate-${index * 45}`}
-                  style={{ transformOrigin: "center center" }}
+                  className={`absolute top-0 left-1/2 h-1/2 -ml-[1.6rem] origin-bottom rotate-${
+                    index * 45
+                  }`}
                 >
-                  <img
-                    className="m-auto"
-                    width={app.width}
-                    height={app.height}
-                    alt={app.title}
-                    src={app.icon}
-                  />
+                  <div
+                    className={`relative -top-[1.6rem] flex w-[3.2rem] h-[3.2rem] bg-n-7 border border-n-1/15 rounded-xl -rotate-${
+                      index * 45
+                    }`}
+                  >
+                    <img
+                      className="m-auto"
+                      width={app.width}
+                      height={app.height}
+                      alt={app.title}
+                      src={app.icon}
+                    />
+                  </div>
                 </li>
               ))}
             </ul>
